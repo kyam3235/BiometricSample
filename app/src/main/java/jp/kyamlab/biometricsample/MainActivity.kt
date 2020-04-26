a97a89b32d9345ecc6c890a445805d44ef68cefc
Kyohei Yamaguchi <kyohei3235@gmail.com>
2020-04-26 20:34:24 +0900

    アプリ起動時に生体認証をおこなうよう変更

--- app/src/main/java/jp/kyamlab/biometricsample/MainActivity.kt
+++ app/src/main/java/jp/kyamlab/biometricsample/MainActivity.kt
@@ -1,12 +1,102 @@
 package jp.kyamlab.biometricsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+
+    private val executor = Executor {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isBiometricManagerAvailable()) {
+            Toast.makeText(
+                applicationContext, "Authentication failed",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        showBiometricPrompt()
+    }
+
+    // デバイスが生体認証をサポートしているか
+    // true:サポート
+    // false:サポートしていない
+    private fun isBiometricManagerAvailable(): Boolean {
+        val biometricManager = BiometricManager.from(this)
+        return when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Log.d(TAG, "App can authenticate using biometrics.")
+                true
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Log.e(TAG, "No biometric features available on this device.")
+                false
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                Log.e(TAG, "Biometric features are currently unavailable.")
+                false
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                Log.e(
+                    TAG,
+                    "The user hasn't associated any biometric credentials with their account."
+                )
+                false
+            }
+            else -> throw RuntimeException()
+        }
+    }
+
+    private fun showBiometricPrompt() {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        val biometricPrompt = BiometricPrompt(
+            this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    val authenticatedCryptoObject: BiometricPrompt.CryptoObject? =
+                        result.cryptoObject
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
+        )
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    companion object {
+        // ログ表示用のタグ
+        private const val TAG = "MainActivity"
     }
 }
